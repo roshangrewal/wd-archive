bda6c7f7ecb17f6c81f7f407c61105c804b8be36
roshangrewal <roshangrewal@hotmail.com>
2020-03-17 13:06:11 +0530

    Old Files Found Somewhere

--- bitcoin-ticker/index.js
+++ bitcoin-ticker/index.js
@@ -14,9 +14,22 @@ app.get('/', function(req, res){
 });
 
 app.post('/', function(req, res){
-  request("https://apiv2.bitcoinaverage.com/indices/global/ticker/all?crypto=BTC&fiat=USD,EUR", function(error, response, body){
-    console.log(body);
-  })
+
+  var crypto = req.body.crypto;
+  var fiat = req.body.fiat;
+
+  var baseURL =  "https://apiv2.bitcoinaverage.com/indices/global/ticker/";
+  var finalURL = baseURL + crypto + fiat;
+
+  request(finalURL, function(error, response, body){
+    var data = JSON.parse(body);
+    var price = data.last;
+    var currentDate = data.display_timestamp;
+
+    res.write("<p>Current date is " + currentDate + "</p>");
+    res.write("<h1>The current price of "+ crypto + " is "+ price +" "+ fiat + "</h1>");
+    res.send();
+  });
 });
 
 
