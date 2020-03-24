23371baa27b348721fc47374f457e7d7f903dfec
roshangrewal <roshangrewal@hotmail.com>
2020-03-24 23:51:21 +0530

    JS date object instead of using switch

--- schedule-of-operation-v1/app.js
+++ schedule-of-operation-v1/app.js
@@ -8,34 +8,43 @@ app.set('view engine', 'ejs');
 app.get('/', function(req, res) {
 
   var today = new Date();
-  var currentDay = today.getDay();
-  var day = "";
 
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-    default:
-    console.log("Error - current day is " + currentDay);
-  }
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  var day = today.toLocaleDateString("en-US", options);
+
+  // var currentDay = today.getDay();
+  // var day = "";
+  //
+  // switch (currentDay) {
+  //   case 0:
+  //     day = "Sunday";
+  //     break;
+  //   case 1:
+  //     day = "Monday";
+  //     break;
+  //   case 2:
+  //     day = "Tuesday";
+  //     break;
+  //   case 3:
+  //     day = "Wednesday";
+  //     break;
+  //   case 4:
+  //     day = "Thursday";
+  //     break;
+  //   case 5:
+  //     day = "Friday";
+  //     break;
+  //   case 6:
+  //     day = "Saturday";
+  //     break;
+  //   default:
+  //   console.log("Error - current day is " + currentDay);
+  // }
 
   res.render('list', {
     theDay: day
