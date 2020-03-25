9cb38251e5c42eddb86b7581c6ac3021ff7bd01f
roshangrewal <roshangrewal@hotmail.com>
2020-03-25 23:42:00 +0530

    added designing part for landing page

--- schedule-of-operation-v1/app.js
+++ schedule-of-operation-v1/app.js
@@ -3,12 +3,16 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+var items = ["Start Scheduling"];
+
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
 
 app.get('/', function(req, res) {
 
   var today = new Date();
-
   var options = {
     weekday: "long",
     day: "numeric",
@@ -16,43 +20,16 @@ app.get('/', function(req, res) {
   };
 
   var day = today.toLocaleDateString("en-US", options);
-
-  // var currentDay = today.getDay();
-  // var day = "";
-  //
-  // switch (currentDay) {
-  //   case 0:
-  //     day = "Sunday";
-  //     break;
-  //   case 1:
-  //     day = "Monday";
-  //     break;
-  //   case 2:
-  //     day = "Tuesday";
-  //     break;
-  //   case 3:
-  //     day = "Wednesday";
-  //     break;
-  //   case 4:
-  //     day = "Thursday";
-  //     break;
-  //   case 5:
-  //     day = "Friday";
-  //     break;
-  //   case 6:
-  //     day = "Saturday";
-  //     break;
-  //   default:
-  //   console.log("Error - current day is " + currentDay);
-  // }
-
-  res.render('list', {
-    theDay: day
-  });
+  res.render('list', { theDay: day, newListItems: items });
 
 });
 
-
+app.post('/', function(req, res){
+  var item = req.body.newItem;
+  items.push(item)
+  res.redirect('/');
+  // console.log(item);
+})
 
 app.listen(3000, function() {
   console.log('Server is running on port 3000');
