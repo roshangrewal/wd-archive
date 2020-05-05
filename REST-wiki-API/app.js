b2337c8f2f040ab037a5c19f9885e429d472a767
roshangrewal <roshangrewal@hotmail.com>
2020-05-05 21:54:28 +0530

    rest api initial setup

---
+++ REST-wiki-API/app.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+mongoose.connect('mongodb://localhost:27017/wikiDB', {useNewUrlParser: true, useUnifiedTopology: true });
+
+const articleSchema = {
+    title: String,
+    content: String
+};
+
+const Article = mongoose.model('Article', articleSchema);
+
+
+app.get('/articles', function(req, res){
+    Article.find(function(err, foundArticle){
+        // console.log(foundArticle);
+        if(!err) {
+            res.send(foundArticle);
+        } else {
+            res.send(err);
+        }
+        
+    })
+})
+
+app.post('/articles', function(req, res){
+    // console.log(req.body.title);
+    // console.log(req.body.content);
+    
+    const newArticle = new Article({
+        title: req.body.title,
+        content: req.body.content
+    });
+    newArticle.save(function(err){
+        if(!err) {
+            res.send('Successfully added new article :) ');
+        } else {
+            res.send(err);
+        }
+    });
+});
+
+app.delete('/articles', function(req, res){
+     
+})
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
