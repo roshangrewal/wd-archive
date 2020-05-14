4549c40363f2540532da3b96a34e6812411f1333
roshangrewal <roshangrewal@hotmail.com>
2020-05-15 01:22:12 +0530

    user signin, signup, signout are working fine now

--- mern-ecom/app.js
+++ mern-ecom/app.js
@@ -3,12 +3,15 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const expressValidator = require('express-validator');
 // load env variables
 const dotenv = require('dotenv');
 dotenv.config();
 
 // import routes
 const userRoutes = require('./routes/user');
+
+const app = express();
 
 //db connection
 mongoose
@@ -19,19 +22,18 @@ mongoose
   })
   .then(() => console.log('DB Connected'));
 
-mongoose.connection.on('error', (err) => {
-  console.log(`DB connection error: ${err.message}`);
-});
-
-const app = express();
+// mongoose.connection.on('error', (err) => {
+//   console.log(`DB connection error: ${err.message}`);
+// });
 
 // middlewares
 app.use(morgan('dev'));
 app.use(bodyParser.json());
-app.use(cookieParser);
+app.use(cookieParser());
+app.use(expressValidator());
 
 // routes middleware
-app.use('/api/', userRoutes);
+app.use('/api', userRoutes);
 
 const port = process.env.PORT || 8000;
 
--- mern-ecom/models/user.js
+++ mern-ecom/models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const crypto = require('crypto');
-const uuidv1 = require('uuid/dist/v1');
+const { v1: uuidv1 } = require('uuid');
 
 const userSchema = new mongoose.Schema(
   {
