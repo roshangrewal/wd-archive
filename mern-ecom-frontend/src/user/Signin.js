ca42d40cee3a165e7effea8431f5e0842a130a22
roshangrewal <roshangrewal@hotmail.com>
2020-05-16 22:20:38 +0530

    pages and layouts completed

---
+++ mern-ecom-frontend/src/user/Signin.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '../core/Layout';
+import { API } from '../config';
+
+const Signin = () => (
+  <Layout title='Signin' description='Signin to Node React Ecommerce App'>
+    {API}
+  </Layout>
+)
+
+export default Signin;
