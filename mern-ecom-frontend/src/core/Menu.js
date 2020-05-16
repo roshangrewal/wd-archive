ee0afb697fa5e687568999b43b2086a465e8b382
roshangrewal <roshangrewal@hotmail.com>
2020-05-17 03:35:31 +0530

    user signin and signup completed

--- mern-ecom-frontend/src/core/Menu.js
+++ mern-ecom-frontend/src/core/Menu.js
@@ -1,32 +1,69 @@
-import React from 'react';
-import { Link, withRouter } from 'react-router-dom';
+import React, { Fragment } from "react";
+import { Link, withRouter } from "react-router-dom";
+import { signout, isAuthenticated } from "../auth";
 
 const isActive = (history, path) => {
-  if (history.location.pathname === path) {
-    return { color: '#ff9900' }
-  } else {
-    return { color: "#ffffff" }
-  }
-}
+    if (history.location.pathname === path) {
+        return { color: "#ff9900" };
+    } else {
+        return { color: "#ffffff" };
+    }
+};
 
-const Menu = (props) => (
-  <div>
-    <ul className="nav nav-tabs bg-primary">
-      <li className="nav-item">
-        <Link className="nav-link" style={isActive(props.history, '/')} to="/">Home</Link>
-      </li>
+const Menu = ({ history }) => (
+    <div>
+        <ul className="nav nav-tabs bg-primary">
+            <li className="nav-item">
+                <Link
+                    className="nav-link"
+                    style={isActive(history, "/")}
+                    to="/"
+                >
+                    Home
+                </Link>
+            </li>
 
-      <li className="nav-item">
-        <Link className="nav-link" style={isActive(props.history, '/signin')} to="/signin">Signin</Link>
-      </li>
+            {!isAuthenticated() && (
+                <Fragment>
+                    <li className="nav-item">
+                        <Link
+                            className="nav-link"
+                            style={isActive(history, "/signin")}
+                            to="/signin"
+                        >
+                            Signin
+                        </Link>
+                    </li>
 
-      <li className="nav-item">
-        <Link className="nav-link" style={isActive(props.history, '/signup')} to="/signup">Signup</Link>
-      </li>
+                    <li className="nav-item">
+                        <Link
+                            className="nav-link"
+                            style={isActive(history, "/signup")}
+                            to="/signup"
+                        >
+                            Signup
+                        </Link>
+                    </li>
+                </Fragment>
+            )}
 
-
-    </ul>
-  </div>
-)
+            {isAuthenticated() && (
+                <li className="nav-item">
+                    <span
+                        className="nav-link"
+                        style={{ cursor: "pointer", color: "#ffffff" }}
+                        onClick={() =>
+                            signout(() => {
+                                history.push("/");
+                            })
+                        }
+                    >
+                        Signout
+                    </span>
+                </li>
+            )}
+        </ul>
+    </div>
+);
 
 export default withRouter(Menu);
