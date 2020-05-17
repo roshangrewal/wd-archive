7590f1d75382835164004d466154551c05b7b49e
roshangrewal <roshangrewal@hotmail.com>
2020-05-18 03:20:29 +0530

    filter by category and price in shop page

--- mern-ecom-frontend/src/Routes.js
+++ mern-ecom-frontend/src/Routes.js
@@ -8,13 +8,15 @@ import Dashboard from "./user/UserDashboard";
 import AdminRoute from "./auth/AdminRoute";
 import AdminDashboard from "./user/AdminDashboard";
 import AddCategory from "./admin/AddCategory";
-import AddProduct from './admin/AddProduct'
+import AddProduct from './admin/AddProduct';
+import Shop from './core/Shop';
 
 const Routes = () => {
     return (
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact component={Home} />
+                <Route path="/shop" exact component={Shop} />
                 <Route path="/signin" exact component={Signin} />
                 <Route path="/signup" exact component={Signup} />
                 <PrivateRoute
