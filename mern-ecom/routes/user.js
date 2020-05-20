5f69c40135998e6d8e7a967789737bfe6ce29c2e
roshangrewal <roshangrewal@hotmail.com>
2020-05-20 14:22:47 +0530

    all api endpoints are working

--- mern-ecom/routes/user.js
+++ mern-ecom/routes/user.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
-
 const { userById, read, update, purchaseHistory } = require('../controllers/user');
 
 router.get('/secret/:userId', requireSignin, isAuth, (req, res) => {
