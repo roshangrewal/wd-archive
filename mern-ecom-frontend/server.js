2d99357deb1db1a2f05a62a7c0998c67228b3648
roshangrewal <roshangrewal@hotmail.com>
2020-05-20 14:24:29 +0530

    manage and update product completed

---
+++ mern-ecom-frontend/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const compression = require('compression');
+const path = require('path');
+const app = express();
+ 
+app.use(compression());
+app.use(express.static(path.join(__dirname, 'build')));
+ 
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+ 
+const PORT = process.env.PORT || 3000;
+ 
+app.listen(PORT, () => {
+    console.log(`App is running on port ${PORT}`);
+});
+
+
+
+// Code nginx sites configuration:
+
+// location /api {
+//     proxy_pass http://localhost:8000;
+//     proxy_http_version 1.1;
+//     proxy_set_header Upgrade $http_upgrade;
+//     proxy_set_header Connection 'upgrade';
+//     proxy_set_header Host $host;
+//     proxy_cache_bypass $http_upgrade;
+// }
+ 
+// location / {
+//     proxy_pass http://localhost:3000;
+//     proxy_http_version 1.1;
+//     proxy_set_header Upgrade $http_upgrade;
+//     proxy_set_header Connection 'upgrade';
+//     proxy_set_header Host $host;
+//     proxy_cache_bypass $http_upgrade;
+// }
